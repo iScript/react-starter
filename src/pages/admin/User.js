716ca1e2d558f0be29fb94774147c2bf41d2243a
yuankeqiang <379395979@qq.com>
2022-08-19 15:35:00 +0800

    Update User.js

--- src/pages/admin/User.js
+++ src/pages/admin/User.js
@@ -1,7 +1,43 @@
-export default function Index() {
+import { Space, Table, Tag } from "antd";
+
+const columns = [
+  {
+    title: "Id",
+    dataIndex: "id",
+  },
+  {
+    title: "用户名",
+    dataIndex: "name",
+  },
+  {
+    title: "操作",
+
+    render: (_, record) => (
+      <Space size="middle">
+        <a>修改 </a>
+        <a>删除</a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: "1",
+    id: 1,
+    name: "John Brown",
+  },
+  {
+    key: "2",
+    id: 2,
+    name: "John Brown",
+  },
+];
+
+export default function User() {
   return (
     <>
-      <div>user..</div>
+      <Table columns={columns} dataSource={data} />
     </>
   );
 }
